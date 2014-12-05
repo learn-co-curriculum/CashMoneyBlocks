5244d08be619aae8dd8586127f83824c1290d693
zdrossman <zachary.drossman@gmail.com>
2014-12-05 10:00:47 -0500

    Updated for issues raised by Joe

--- CashMoneyBlocks/Transaction.m
+++ CashMoneyBlocks/Transaction.m
@@ -15,7 +15,6 @@
 - (instancetype)initWithProduct:(Product *)product Quantity:(NSNumber *)quantity Date:(NSDate *)dateOfTransaction
 {
     
-    [[NSNumber numberWithFloat:23.0] stringValue];
     self = [super init];
     if (self) {
         _product = product;
